06de696bca74807a88bc896198d8caa53aab713f
kamleshBisht04 <kamleshbisht04@gmail.com>
2026-03-27 16:33:28 +0530

    how callBack hell occour in async code '

---
+++ index.js
@@ -0,0 +1,19 @@
+// callback hell in async code  
+
+const fs = require('fs');
+const superagent = require('superagent');
+
+fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+  console.log(`Breed :  ${data}`);
+  superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .end((err, res) => {
+      if (err) return console.log(err.message);
+      console.log(res.body.message);
+      //  agian call back write
+
+      fs.writeFile('dog-img.txt', res.body.message, (err) => {
+        console.log('Random dog image saved to file!');
+      });
+    });
+});
