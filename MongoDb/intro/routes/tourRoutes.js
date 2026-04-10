0cb8bacecd8ea8d5fd9b52f5f68e1b7c66ea05f9
kamleshBisht04 <kamleshbisht04@gmail.com>
2026-04-10 09:21:42 +0530

    learning exprss and fundamentals

---
+++ MongoDb/intro/routes/tourRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const tourControllers = require('./../controllers/tourControllers');
+
+router.param('id', (req, res, next, val) => {
+  console.log(`Tour id is ${val}`);
+  next();
+});
+
+router
+  .route(`/`)
+  .get(tourControllers.getAllTours)
+  .post(tourControllers.checkBody, tourControllers.createTour);
+router
+  .route(`/:id`)
+  .get(tourControllers.getTour)
+  .patch(tourControllers.updateTour)
+  .delete(tourControllers.deleteTour);
+
+module.exports = router;
