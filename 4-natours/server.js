4647673ca07e0d1143211b9aac391b897cdd9d03
kamleshBisht04 <kamleshbisht04@gmail.com>
2026-04-10 09:20:10 +0530

    connecting the mongo db with express

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,6 +1,20 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
-const port = 3000;
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose.connect(DB).then((con) => {
+  console.log(con.connections);
+  console.log('DB connection successful!');
+});
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App Running on port no ${port}...`);
 });
